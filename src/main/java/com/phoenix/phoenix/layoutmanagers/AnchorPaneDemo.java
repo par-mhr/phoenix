0855e3e1aac59741a11ea71c902ca3b1440fc700
par-mhr <paras.maharjan10@gmail.com>
2023-08-10 10:46:21 +0545

    Added Anchor Pane

---
+++ src/main/java/com/phoenix/phoenix/layoutmanagers/AnchorPaneDemo.java
@@ -0,0 +1,38 @@
+package com.phoenix.phoenix.layoutmanagers;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.TilePane;
+import javafx.stage.Stage;
+
+public class AnchorPaneDemo extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        AnchorPane anchorPane = new AnchorPane();
+        TextArea textArea = new TextArea();
+        Button button = new Button("Submit");
+
+        anchorPane.getChildren().addAll(textArea, button);
+        AnchorPane.setTopAnchor(textArea, 10.0);
+        AnchorPane.setLeftAnchor(textArea, 10.0);
+        AnchorPane.setBottomAnchor(textArea, 10.0);
+        AnchorPane.setRightAnchor(textArea, 100.0);
+
+
+
+        AnchorPane.setRightAnchor(button, 10.0);
+        AnchorPane.setTopAnchor(button, 10.0);
+
+
+        Scene scene = new Scene(anchorPane, 600, 400);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
